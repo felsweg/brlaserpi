31847f925279eb4af6323a2aff0d5198bd5d74eb
felsweg <23339634+felsweg@users.noreply.github.com>
2024-01-06 21:46:31 +0100

    Update setup.sh
    adding catfood

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -euo pipefail
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y cups printer-driver-brlaser
+sudo apt install -y cups printer-driver-brlaser neovim
 sudo usermod -aG lpadmin $USER
 sudo systemctl restart cups
