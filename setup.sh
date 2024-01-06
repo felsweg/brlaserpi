ef9732e9211f27c1cffd7b4140f3ad5c518f3459
felsweg <23339634+felsweg@users.noreply.github.com>
2024-01-06 21:24:59 +0100

    Update setup.sh
    catto cat

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y cups printer-driver-brlaser
 sudo usermod -aG lpadmin $USER
-sudo systemctl rerestrt cups
+sudo systemctl restart cups
