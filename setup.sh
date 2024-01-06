84b4be2dffe697c8c8222ac6c3cf4aa64d2af3a1
felsweg <23339634+felsweg@users.noreply.github.com>
2024-01-06 21:07:50 +0100

    Update setup.sh
    laz0rcats

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 set -euo pipefail
 sudo apt update && sudo apt upgrade -y
-sudo usermod -aG lpadmin $USER
 sudo apt install -y cups printer-driver-brlaser
+sudo usermod -aG lpadmin $USER
+sudo systemctl rerestrt cups
